92272bb7c3b8f5124b1486eca371a7d094102cf6
kiran137 <34226856+kiran137@users.noreply.github.com>
2018-07-17 17:54:03 +0530

    Update stock.py

--- Stock-Prediction/stock.py
+++ Stock-Prediction/stock.py
@@ -12,29 +12,8 @@ import seaborn as sns
 from matplotlib.pylab import rcParams
 import calendar
 import datetime as dt
-import types
-import tempfile
-
-def make_keras_picklable():
-    def __getstate__(self):
-        model_str = ""
-        with tempfile.NamedTemporaryFile(suffix='.hdf5', delete=True) as fd:
-            keras.models.save_model(self, fd.name, overwrite=True)
-            model_str = fd.read()
-        d = { 'model_str': model_str }
-        return d
-
-    def __setstate__(self, state):
-        with tempfile.NamedTemporaryFile(suffix='.hdf5', delete=True) as fd:
-            fd.write(state['model_str'])
-            fd.flush()
-            model = keras.models.load_model(fd.name)
-        self.__dict__ = model.__dict__
 
 
-    cls = keras.models.Model
-    cls.__getstate__ = __getstate__
-    cls.__setstate__ = __setstate__
 
 #read dataset
 dataset = pd.read_csv('NSE-HITECH.csv')
@@ -137,10 +116,4 @@ from keras.models import load_model
 
 from sklearn.pipeline import make_pipeline
 from sklearn.externals import joblib
-
-#load_model(model,'ann_model.h5')
-import pickle
-
-make_keras_picklable()
-
-pickle.dumps(model)
+joblib.dump(model,'ann_model.pkl')
